a84298fcf8aecf0e4dc154aed3594848d175c827
codonculous <feidameng01@gmail.com>
2017-08-22 23:48:24 -0400

    Created connect and disconnect event handler using websocket

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
-const path = require('path');
-const publicPath = path.join(__dirname, '../public')
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
 
-const app = express();
+const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
+const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
+io.on("connection", (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('Disconnected !!!!!!');
+  });
+});
 
 
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });

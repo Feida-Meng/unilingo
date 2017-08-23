55cb5213f648fbe12f4cf4f313cea2e0a52173f5
codonculous <feidameng01@gmail.com>
2017-08-22 22:16:59 -0400

    Setup express

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public')
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
